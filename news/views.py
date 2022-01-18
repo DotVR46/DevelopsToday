68a6fac4247811f887fdf16819388abfccdf9f52
Дмитрий Макаров <maggot8950@gmail.com>
2022-01-18 18:19:35 +0600

    drf-yasg, docstrings, custom permissions

--- news/views.py
+++ news/views.py
@@ -4,44 +4,71 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 
 from news.models import News
+from news.permissions import IsOwnerOrReadOnly
 from news.serializers import NewsListSerializer, NewsDetailSerializer
 
 
 class CustomPagination(PageNumberPagination):
+    """
+    Пагинация для списка новостей и списка комментариев
+    """
     page_size = 10
     page_query_param = 'p'
 
 
 class NewsListView(ListAPIView):
+    """
+    Список всех новостей с пагинацией
+    """
+    queryset = News.objects.all()
     serializer_class = NewsListSerializer
     pagination_class = CustomPagination
-    queryset = News.objects.all()
 
 
 class NewsDetailView(RetrieveAPIView):
+    """
+    Детальный просмотр новости по id
+    """
+    queryset = News.objects.all()
     serializer_class = NewsDetailSerializer
     lookup_field = 'id'
-    queryset = News.objects.all()
 
 
 class NewsDeleteView(DestroyAPIView):
-    serializer_class = NewsDetailSerializer
-    lookup_field = 'id'
+    """
+    Удаление новости по id. Удалять может только owner
+    """
     queryset = News.objects.all()
+    serializer_class = NewsDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    lookup_field = 'id'
 
 
 class NewsUpdateView(UpdateAPIView):
-    serializer_class = NewsDetailSerializer
-    lookup_field = 'id'
+    """
+    Изменение новости по id. Изменять может только owner
+    """
     queryset = News.objects.all()
+    serializer_class = NewsDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    lookup_field = 'id'
 
 
 class NewsCreateView(CreateAPIView):
-    serializer_class = NewsDetailSerializer
+    """
+    Создание объекта новости. Нужно указать автора
+    """
     queryset = News.objects.all()
+    serializer_class = NewsDetailSerializer
 
 
 def vote_news(request, id):
+    """
+    Эндпоинт для голосования
+    :param request:
+    :param id:
+    :return:
+    """
     news = get_object_or_404(News, id=id)
     news.upvotes += 1
     news.save()
