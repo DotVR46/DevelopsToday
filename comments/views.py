4f442972d76b2605352e80c16e906933c81bb6a3
Дмитрий Макаров <maggot8950@gmail.com>
2022-01-18 15:03:10 +0600

    periodic tasks, app:comments - views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, RetrieveAPIView, UpdateAPIView
 
-# Create your views here.
+from comments.models import Comment
+from comments.serializers import CommentSerializer
+from news.views import CustomPagination
+
+
+class CommentListView(ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = CustomPagination
+
+
+class CommentCreateView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDeleteView(DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'id'
+
+
+class CommentDetailView(RetrieveAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'id'
+
+
+class CommentUpdateView(UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'id'
